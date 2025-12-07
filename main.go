d28b9d10e1d228305f1c962f25ab04d1c756c568
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-12-06 23:03:37 -0300

    rethinking architecture

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/Aragon-Joaquin/curlWrapper_CLI/logger"
-	"github.com/Aragon-Joaquin/curlWrapper_CLI/ui"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-type Application struct {
-	tv *tview.Application
-	UI *ui.UIService
-}
+const (
+	APP_NAME            = "Curl Wrapper"
+	DEFAULT_URL_EXAMPLE = "http://localhost:5172"
+)
+
+var (
+	HTTP_METHODS = []string{"GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"}
+)
 
 func main() {
 	err := logger.Load(logger.DefaultPath(), slog.LevelDebug)
@@ -21,18 +25,64 @@ func main() {
 		panic(err)
 	}
 
-	drawView := tview.NewApplication()
-	app := &Application{
-		tv: drawView,
-		UI: ui.CreateUIService(drawView),
-	}
+	slog.Debug("App is running normally")
+
+	//! creating the layout
+	app := tview.NewApplication()
+	app.EnableMouse(true) // not that im going to use this
+	app.SetTitle(APP_NAME)
 
 	tview.Styles = tview.Theme{
-		TitleColor: tcell.ColorChartreuse,
+		// input
+		ContrastBackgroundColor:    tcell.ColorDarkRed,    //bg-color
+		PrimaryTextColor:           tcell.ColorWhiteSmoke, //text-color
+		ContrastSecondaryTextColor: tcell.ColorDimGray,    // placeholder-color
+		SecondaryTextColor:         tcell.ColorWhiteSmoke, //label-color
+
+		TitleColor:                  tcell.ColorGreenYellow,
+		MoreContrastBackgroundColor: tcell.ColorDarkRed, // menus-bg-color
+		BorderColor:                 tcell.ColorDarkSlateGray,
 	}
 
-	mainBox := app.UI.GenerateLayout()
-	if err := app.tv.SetRoot(mainBox, true).SetFocus(mainBox).Run(); err != nil {
+	//creating the form
+
+	urlInput := tview.NewInputField().
+		SetLabel("URL: ").
+		SetPlaceholder(DEFAULT_URL_EXAMPLE).
+		SetFieldWidth(30).
+		SetAcceptanceFunc(nil).
+		SetChangedFunc(func(text string) {
+			slog.Debug("Input Text Changed: ", "TEXT", text)
+		})
+
+	methodDMenu := tview.NewDropDown().
+		SetLabel("HTTP Method: ").
+		SetOptions(HTTP_METHODS, func(text string, index int) {
+			slog.Debug("DropDown Changed: ", "TEXT", text, "INDEX", index)
+		}).
+		SetCurrentOption(0)
+
+	form := tview.NewForm().
+		AddFormItem(urlInput).
+		AddFormItem(methodDMenu).
+		AddButton("Save", func() {
+			app.SetTitle("Saved!")
+			time.AfterFunc(time.Second*1, func() {
+				app.SetTitle(APP_NAME)
+			})
+		}).
+		AddButton("Quit", func() {
+			app.Stop()
+		}).
+		SetButtonsAlign(tview.AlignCenter)
+
+	form.SetBorder(true).SetTitle(APP_NAME)
+	// final layout
+	mainBox := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(form, 0, 1, true).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("JSON Response"), 0, 1, false)
+
+	if err := app.SetRoot(mainBox, true).Run(); err != nil {
 		slog.Error(err.Error())
 	}
 }
--- ui/ui.go
+++ ui/ui.go
@@ -9,23 +9,3 @@ type UIService struct {
 func CreateUIService(t *tview.Application) *UIService {
 	return &UIService{t}
 }
-
-func (t *UIService) GenerateLayout() *tview.Grid {
-	newPrimitive := func(text string) tview.Primitive {
-		return tview.NewTextView().
-			SetTextAlign(tview.AlignCenter).
-			SetText(text)
-	}
-
-	main := newPrimitive("Make the request here")
-	side := newPrimitive("JSON Response")
-
-	grid := tview.NewGrid().
-		SetRows().
-		SetColumns(0, 70).
-		SetBorders(true).
-		AddItem(main, 0, 0, 1, 1, 0, 0, false).
-		AddItem(side, 0, 1, 1, 1, 0, 0, false)
-
-	return grid
-}
